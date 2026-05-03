28fe6f327be6cfb576f04a2c24288974c2d02592
Leco Arrindell <79813285+lecoa@users.noreply.github.com>
2026-05-03 21:32:21 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
-  publicPath: '/',
+  // When deploying to a subdirectory (e.g. https://larrindell.com/jwvids/),
+  // set publicPath to the subdirectory so asset URLs are generated correctly.
+  publicPath: '/jwvids/',
 };
